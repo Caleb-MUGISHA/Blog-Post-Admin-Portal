995afeb2fb7223ac744e5cd8de94495072d73ead
Caleb-LeGrand12 <c.mugisha@alustudent.com>
2021-06-21 20:22:57 +0200

    arrangment

--- src/App.js
+++ src/App.js
@@ -1,30 +1,10 @@
-import React from "react";
-import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
-import { AddUser } from "./features/users/AddUser";
-import { EditUser } from "./features/users/EditUser";
-import { UserList } from "./features/users/UserList";
-import { Provider } from "react-redux";
-import store from "./store";
-
+import "./App.css";
+import UserManagement from "./components/UserManagement";
 function App() {
   return (
-    <Provider store={store}>
-      <Router>
-        <div>
-          <Switch>
-            <Route path="/add-user">
-              <AddUser />
-            </Route>
-            <Route path="/edit-user">
-              <EditUser />
-            </Route>
-            <Route path="/">
-              <UserList />
-            </Route>
-          </Switch>
-        </div>
-      </Router>
-    </Provider>
+    <div className="app">
+      <UserManagement />
+    </div>
   );
 }
 
